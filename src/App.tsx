41536f6d724f20cad81057e0562db815db647736
Connor <kor.connor@gmail.com>
2025-06-15 00:19:06 +0900

    태그 추가

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,19 @@ import "./App.css";
 function App() {
   return (
     <>
+
       <button
         className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
         onClick={() => alert("Button Clicked!")}
       >
         Click Me
       </button>
+      <div className="px-6 py-4">
+      <div className="font-bold text-xl mb-2">Card Title</div>
+      <p className="text-gray-700 text-base">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+      <span className="">Tag 1</span>
+      <span className="">Tag 2</span>
     </>
   );
 }
